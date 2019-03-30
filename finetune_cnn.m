c5a56daae006edf4a1abda648356380877679132
AxelBremer <axelbremer94@gmail.com>
2019-03-30 20:39:30 +0100

    finished getIMDB

--- finetune_cnn.m
+++ finetune_cnn.m
@@ -1,8 +1,8 @@
 function [net, info, expdir] = finetune_cnn(varargin)
 
 %% Define options
-run(fullfile(fileparts(mfilename('fullpath')), ...
-  '..', '..', '..', 'matlab', 'vl_setupnn.m')) ;
+% run(fullfile(fileparts(mfilename('fullpath')), ...
+%   '..', '..', '..', 'matlab', 'vl_setupnn.m')) ;
 
 opts.modelType = 'lenet' ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
@@ -82,10 +82,46 @@ function imdb = getIMDB()
 classes = {'airplanes', 'birds', 'ships', 'horses', 'cars'};
 splits = {'train', 'test'};
 
+tr_dat = load('stl10_matlab/train.mat');
+classes = [1, 2, 3, 7, 9];
+tr_X = tr_dat.X(ismember(tr_dat.y, classes),:);
+tr_X = reshape(tr_X,[2500,96,96,3]);
+tr_y = tr_dat.y(ismember(tr_dat.y, classes),:);
+tr_y(tr_y == 3) = 5;
+tr_y(tr_y == 7) = 4;
+tr_y(tr_y == 9) = 3;
+
+te_dat = load('stl10_matlab/test.mat');
+classes = [1, 2, 3, 7, 9];
+te_X = te_dat.X(ismember(te_dat.y, classes),:);
+te_X = reshape(te_X,[4000,96,96,3]);
+te_y = te_dat.y(ismember(te_dat.y, classes),:);
+te_y(te_y == 3) = 5;
+te_y(te_y == 7) = 4;
+te_y(te_y == 9) = 3;
+
+%keyboard
+data = zeros([32,32,3,6500]);
+sets = ones([6500,1]);
+sets(2501:6500) = 2;
+labels = cat(1, tr_y, te_y);
 %% TODO: Implement your loop here, to create the data structure described in the assignment
 %% Use train.mat and test.mat we provided from STL-10 to fill in necessary data members for training below
 %% You will need to, in a loop function,  1) read the image, 2) resize the image to (32,32,3), 3) read the label of that image
+n = size(tr_y,1);
 
+for i = 1:n
+    im = squeeze(tr_X(i,:,:,:));
+    im = imresize(im, [32,32]);
+    data(:,:,:,i) = im;
+end
+
+for i = 1:size(te_y,1)
+    j = i + n;
+    im = squeeze(te_X(i,:,:,:));
+    im = imresize(im, [32,32]);
+    data(:,:,:,j) = im;
+end
 %%
 % subtract mean
 dataMean = mean(data(:, :, :, sets == 1), 4);
--- main.m
+++ main.m
@@ -1,6 +1,5 @@
 %% main function 
 
-
 %% fine-tune cnn
 
 [net, info, expdir] = finetune_cnn();
@@ -8,10 +7,11 @@
 %% extract features and train svm
 
 % TODO: Replace the name with the name of your fine-tuned model
-nets.fine_tuned = load(fullfile(expdir, 'your_new_model.mat')); nets.fine_tuned = nets.fine_tuned.net;
+%nets.fine_tuned = load(fullfile(expdir, 'your_new_model.mat')); nets.fine_tuned = nets.fine_tuned.net;
 nets.pre_trained = load(fullfile('data', 'pre_trained_model.mat')); nets.pre_trained = nets.pre_trained.net; 
-data = load(fullfile(expdir, 'imdb-stl.mat'));
+%data = load(fullfile(expdir, 'imdb-stl.mat'));
 
+vl_simplenn_display(nets.pre_trained);
 
 %%
 train_svm(nets, data);
